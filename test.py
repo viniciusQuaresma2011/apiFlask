f328d1aa62fb9ce72f809e22eabdba08b45e2a86
unknown <viniciusquaresma14@hotmail.com>
2022-10-20 15:25:42 -0300

    atualizacao

--- test.py
+++ test.py
@@ -1,13 +1,12 @@
-from array import array
-from http.client import ACCEPTED
+
 from urllib import request, response
 import requests
-import json
 from flask import jsonify
 
 BASE = "http://127.0.0.1:5000/"
 
 headers = {'accept': 'application/json'}
+
 payload = {"name": "tiziu", "views": 4, "likes": 10}
 
 
@@ -15,17 +14,22 @@ data = [{"name": "tiziu1", "views": 5, "likes": 11},
         {"name": "tiziu2", "views": 6, "likes": 12},
         {"name": "tiziu3", "views": 9, "likes": 15}]
 
-for i in range(data):
-    response = requests.put(BASE + 'video/' + str(i), data=[i.])
-    print(response.json())
+
+
+for i in enumerate(data):
+    response = requests.put(BASE + 'video/' + str(i), data=[i])
+    print(response)
 
 input()
 
 # response = requests.delete(BASE + 'video/0')
 # print(response.json())
 
-response = requests.get(BASE + '/video/list')
-print(response.json())
+
+# response = requests.get(BASE + "video/6")
+# print(response.json())
+
+
 
 
 
